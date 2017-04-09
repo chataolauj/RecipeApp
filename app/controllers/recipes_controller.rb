9ec3621240c2917f39f0dde30513088d9e3d98c0
Toubee Lo <tlo4@uncc.edu>
2017-04-09 22:26:29 +0000

    Complete CRUD actions

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -20,6 +20,27 @@ class RecipesController < ApplicationController
             render 'new'
         end
     end
+    
+    def edit
+        @recipe = Recipe.find(params[:id])
+    end
+    
+    def update
+        @recipe = Recipe.find(params[:id])
+        
+        if @recipe.update(recipe_params)
+            redirect_to @recipe
+        else
+            render 'edit'
+        end
+    end
+    
+    def destroy
+        @recipe = Recipe.find(params[:id])
+        @recipe.destroy
+        
+        redirect_to recipes_path
+    end
 end
 
 private
