c9d8e3976a0b10d50f0b3d944a62ff2dca1bda2c
Toubee Lo <tlo4@uncc.edu>
2017-04-10 05:51:27 +0000

    Added comment form to show view

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,12 @@
 class CommentsController < ApplicationController
+    def create
+        @recipe = Recipe.find(params[:recipe_id]) #retrieves the current article and saves it to the @article object
+        @comment = @recipe.comments.create(comments_params) #creates the @comments object with the parameters passed to it by the view
+        redirect_to recipe_path(@recipe) #redirects back to the page with the article and new comments
+    end
+    
+    private
+        def comments_params
+            params.require(:comment).permit(:name, :comment, :rating)
+        end
 end
